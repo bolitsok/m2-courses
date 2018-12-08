f5153e36c167aaf7cfcd5a870075e658d7884f56
alexander <you@example.com>
2018-12-08 12:34:10 +0200

    custom command module

--- app/etc/config.php
+++ app/etc/config.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'modules' => [
-        'Alexander_M2-courses' => 1,
+        'Academy_HelloWord' => 1,
         'Magento_Store' => 1,
         'Magento_Directory' => 1,
         'Magento_Eav' => 1,
