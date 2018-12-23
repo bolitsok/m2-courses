88720fa7ce5973d3c9ddaec93d6551c0b7d69f75
alexander <you@example.com>
2018-12-23 12:24:07 +0200

    routes admin

---
+++ app/code/Academy/Routers/Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Academy\Routers\Controller\Adminhtml\Index;
+
+class Index extends \Magento\Backend\App\Action
+{
+    public function execute()
+    {
+        $x = 1;
+    }
+}
